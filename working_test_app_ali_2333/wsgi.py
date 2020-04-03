23655bad631337f3c7830f2d81c424eb5616ab0f
Crowdbotics <team@crowdbotics.com>
2020-04-03 19:15:54 +0000

    Update models

--- working_test_app_ali_2333/wsgi.py
+++ working_test_app_ali_2333/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'working_test_app_ali_2333.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "working_test_app_ali_2333.settings")
 
 application = get_wsgi_application()
